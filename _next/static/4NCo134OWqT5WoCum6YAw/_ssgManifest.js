9224b71698037bc5994a7e74431fce6f31c9e2c3
Hazem-Alyaari <Hazem-Alyaari@users.noreply.github.com>
2026-09-21 20:46:20 +0000

    deploy: d8ebdd00b6b78e89f9599db953149f43cc0135a6

---
+++ _next/static/4NCo134OWqT5WoCum6YAw/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fblog","\u002F[locale]\u002Fblog\u002F[slug]","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Fdemo","\u002F[locale]\u002Ffaq","\u002F[locale]\u002Ffeatures","\u002F[locale]\u002Fpricing","\u002F[locale]\u002Fsolutions"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
